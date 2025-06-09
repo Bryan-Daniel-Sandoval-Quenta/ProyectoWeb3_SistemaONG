b92a8b7a87f6cd22942dc142acfc89a56d6ddf99
Daniel Sandoval <dan.san.q.gt@gmail.com>
2025-06-08 20:01:12 -0400

    2. agregacion de las aplicaciones a settings, creacion de urls para cada modulo

--- SisDonacionONG/SisDonacionONG/settings.py
+++ SisDonacionONG/SisDonacionONG/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'campania',
+    'donacion',
+    'donante',
+    'ong',
+    'transaccion',
 ]
 
 MIDDLEWARE = [
