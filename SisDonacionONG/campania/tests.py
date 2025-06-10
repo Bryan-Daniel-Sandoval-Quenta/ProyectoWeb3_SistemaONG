8618b61a5316bfe3616029a1580a72e58583fbc2
Bryan Daniel Sandoval Quenta <dan.san.q.gt@gmail.com>
2025-06-09 21:10:08 -0400

    Campania: forms, migracion inicial, templates, y modificaciones en modelo, views, urls y test

--- SisDonacionONG/campania/tests.py
+++ SisDonacionONG/campania/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Campania
+from datetime import date
 
-# Create your tests here.
+class CampaniaTestCase(TestCase):
+    def test_crear_campania(self):
+        campania = Campania.objects.create(
+            titulo="Campaña Escolar",
+            descripcion="Recaudación para útiles",
+            meta_financiera=1000,
+            fecha_inicio=date.today(),
+            fecha_limite=date(2025, 12, 31),
+            estado='A'
+        )
+        self.assertEqual(campania.estado, 'A')
--- SisDonacionONG/campania/views.py
+++ SisDonacionONG/campania/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from .models import Campania
+from .forms import CampaniaForm
 
-# Create your views here.
+class CrearCampaniaView(CreateView):
+    model = Campania
+    form_class = CampaniaForm
+    template_name = 'campania/crear_campania.html'
+    success_url = reverse_lazy('lista_campanias')
+
+class ListaCampaniasView(ListView):
+    model = Campania
+    template_name = 'campania/lista_campanias.html'
+    context_object_name = 'campanias'
