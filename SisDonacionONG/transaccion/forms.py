599d911316d609b6ed986b352128f9d77bbb9e77
Heidy Berenice Mariscal Herrera <hbmariscal@umsa.bo>
2025-06-15 18:19:08 -0400

    ONG: se realizo una agregacion de carpeta static que seria carpeta de estilo,con una carpeta mas de css y un archivo main.css

--- SisDonacionONG/transaccion/forms.py
+++ SisDonacionONG/transaccion/forms.py
@@ -1,9 +1,11 @@
-
-
 from django import forms
-from .models import Transaccion # <-- Esta importación es CORRECTA y NECESARIA
-
+from .models import Transaccion 
 class TransaccionForm(forms.ModelForm):
     class Meta:
         model = Transaccion
-        fields = ['codigo_transaccion', 'proveedor_Pago', 'fecha_Procesamiento', 'estado', 'comision']
+        fields = '__all__' 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, (forms.TextInput, forms.NumberInput, forms.EmailInput, forms.Textarea, forms.Select, forms.DateInput)): # Añade más tipos si los usas
+                field.widget.attrs['class'] = 'form-control'
