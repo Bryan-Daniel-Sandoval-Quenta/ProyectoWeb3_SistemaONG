2502e0c85bf2621117291033b145881e5057e066
Heidy Berenice Mariscal Herrera <hbmariscal@umsa.bo>
2025-06-15 23:00:40 -0400

    TRANSACCION: Se agrego una lista de donantes donde se muestra cuantas transacciones iso un donate

--- SisDonacionONG/transaccion/views.py
+++ SisDonacionONG/transaccion/views.py
@@ -3,6 +3,9 @@ from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from .models import Transaccion
 from .forms import TransaccionForm
+from donante.models import Donante
+from django.db.models import Count
+
 class CrearTransaccionView(CreateView):
     model = Transaccion
     form_class = TransaccionForm
@@ -13,3 +16,11 @@ class ListaTransaccionesView(ListView):
     template_name = 'Transaccion/lista_transaccion.html'
     context_object_name = 'transacciones'
     paginate_by = 10
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        donantes_con_conteo = Donante.objects.annotate(
+            num_transacciones=Count('transaccion') 
+        )
+        context['donantes'] = donantes_con_conteo 
+
+        return context
