7e8df5a7bb8c3a13482616329d62b047915fb7f7
Bryan Daniel Sandoval Quenta <dan.san.q.gt@gmail.com>
2025-06-16 15:33:49 -0400

    8. configurando navbar del inicio, enrutamiento directo a incio en urls

--- SisDonacionONG/SisDonacionONG/urls.py
+++ SisDonacionONG/SisDonacionONG/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [   
     path('admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     path('', include('transaccion.urls')),
     path('', include('donante.urls')),
     path('', include('donacion.urls')),      
+    path('', RedirectView.as_view(url='/inicio/', permanent=False)),
 ]
 
